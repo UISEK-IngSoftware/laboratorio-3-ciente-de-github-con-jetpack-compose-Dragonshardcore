d9ac1896f7d8828ccbab114db0f4871da57361b2
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2026-05-13 11:27:32 -0500

    Laboratorio 5- formularios

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -4,25 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ec.edu.uisek.githubclient.ui.screens.RepoForm
 import ec.edu.uisek.githubclient.ui.screens.RepoList
 import ec.edu.uisek.githubclient.ui.theme.GithubClientTheme
+import ec.edu.uisek.githubclient.viewmodels.RepoListViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             GithubClientTheme {
-                    RepoList()
+                val listViewModel: RepoListViewModel = viewModel()
+
+                var currentScreen by remember {
+                    mutableStateOf("RepoList")
+                }
+
+                when (currentScreen) {
+
+                    "RepoList" -> RepoList(
+                        onNavigateToForm = {
+                            currentScreen = "RepoForm"
+                        }
+                    )
+
+                    "RepoForm" -> RepoForm(
+                        onSaveSuccess = {
+                            listViewModel.fetchRepos()
+                            currentScreen = "RepoList"
+                        },
+                        onBackClick = {
+                            currentScreen = "RepoList"
+                        }
+                    )
                 }
             }
         }
     }
-
+}
